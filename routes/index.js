68972070f0f853653495622dffa968c3ac41ff28
surendrapinninti <34613906+surendrapinninti@users.noreply.github.com>
2019-07-13 21:00:57 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var users=require('../controllers/users.controller');
+var users=require('../userdatacontrollers/users.controller');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
